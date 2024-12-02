f5592bcebf6b5b945a7656e92dfafaba97b69448
Akinmoyero <akinmoyeroboluwatife3@gmail.com>
2024-12-02 15:29:06 +0300

    update innit

---
+++ index2.js
@@ -0,0 +1,13 @@
+function carSpeed (speed) {
+    const speedLimit = 70; // it ensures that the speed limit 70 
+    const perKm = 5;
+    
+}
+if (speed <=speedLimit){
+    console.log ("OK")
+}else{
+    const demeritPoint= Math.floor ((speed - speedLimit)/perKm);// it to calacule the demeritPoint
+    if (demeritPoint >12){
+        console.log("license suspended");
+    }else{console.log(`point: ${demeritPoint}`)}
+}
